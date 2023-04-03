0f116c654f34d0c143dad63335a174726188f0e8
Khairul-Basar <khairulbasar130@gmail.com>
2023-04-03 11:08:28 +0600

    relating data in graphQL

--- index.js
+++ index.js
@@ -130,11 +130,13 @@ const typeDefs = gql`
     price: Float!
     image: String!
     onSale: Boolean!
+    category: Category
   }
 
   type Category {
     id: ID!
     name: String!
+    products: [Product!]!
   }
 `;
 
@@ -161,6 +163,18 @@ const resolvers = {
     category: (parent, args, context) => {
       const { id } = args;
       return categories.find((category) => category.id === id);
+    },
+  },
+  Category: {
+    products: (parent, args, context) => {
+      const categoryId = parent.id;
+      return products.filter((product) => product.categoryId === categoryId);
+    },
+  },
+  Product: {
+    category: (parent, args, context) => {
+      const categoryId = parent.categoryId;
+      return categories.find((category) => category.id === categoryId);
     },
   },
 };
